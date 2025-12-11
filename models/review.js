5ad3ce7bddc4a330aa46754b978e289e40a13189
VasuKakadiya20 <vasukakadiya1234@gmail.com>
2025-12-11 16:54:58 +0530

    4 commit

--- models/review.js
+++ models/review.js
@@ -4,7 +4,7 @@ const reviewschema = new mongoose.Schema({
     userid: {type:mongoose.Schema.Types.ObjectId, ref:"Client", required:true},
     Name: { type:String, required:true},
     Email: {type:String, required:true},
-    Rating: {type:Number, required:true},
+    Rating: {type:Number, required:false},
     Review_Title: {type:String, required:true},
     Review_msg: { type:String, required:true},
     Date:{type:String,required:false}
